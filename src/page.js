061914c98370bdc112a82ed6c03d3de90d59a92c
adiyakhuu1 <my.as.glpzghoo@gmail.com>
2024-12-12 20:29:11 +0800

    Add files via upload

---
+++ src/page.js
@@ -0,0 +1,51 @@
+"use client";
+import Image from "next/image";
+import { useEffect } from "react";
+import { SearchInput } from "./component/SearchInput";
+import { Card } from "./component/card";
+import { WhiteCircle } from "./component/WhiteCircle";
+// import "./App.css";
+
+export default function Home() {
+  // useEffect(() => {
+  //   fetch();
+  // });
+  const Main = () => {
+    return (
+      <div className="flex w-[auto] h-[1200px] justify-content-center relative">
+        <div className="w-[50%] h-[1200px] bg-white relative">
+          <div className="absolute top-10 left-10">
+            <SearchInput />
+          </div>
+          <Card
+            color="white"
+            from="from-slate-200"
+            to="to-white"
+            temp="26"
+            status="./img/sunny.png"
+          />
+        </div>
+
+        <div className="w-[50%] h-[1200px] bg-[#0f141e] relative">
+          <Card
+            color="black"
+            from="from-[#1f2937]"
+            to="to-[#111827]"
+            textColor="text-white"
+            temp="14"
+            status="./img/moon.png"
+          />
+        </div>
+
+        <WhiteCircle size="140px" />
+        <WhiteCircle size="340px" />
+      </div>
+    );
+  };
+  return (
+    <div>
+      <Main />
+      {/* <Main mode={"white"} /> */}
+    </div>
+  );
+}
